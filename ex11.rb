bb9f67ce73aa93cfc36cfdf284ea41ff88c36a13
AVacAdok <avakuang8@gmail.com>
2022-07-22 22:04:32 +0000

    asking question

---
+++ ex11.rb
@@ -0,0 +1,11 @@
+#print let you print out the info/sting on the terminal
+print "How old are you? "
+# get.chomp allows you to set the variable to the input you type in
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+print "what is your fav food?"
+food = gets.chomp
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy. Your fav food is #{food}"
