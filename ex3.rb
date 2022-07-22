6353b623e2ca849d811a2750e6eaabda7de618c1
AVacAdok <avakuang8@gmail.com>
2022-07-22 21:14:10 +0000

    numbers and math

---
+++ ex3.rb
@@ -0,0 +1,25 @@
+puts "I will now count my chickens:"
+
+#calculate the math inside the #{...}
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+# 1. multiplication and division (M&D), from left to right
+# 2. addition and subtraction in one step from left to right
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
