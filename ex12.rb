d567ab048cbda7d7bea860352c7875aca08c3f62
AVacAdok <avakuang8@gmail.com>
2022-07-22 22:16:31 +0000

    prompting ppl for num

---
+++ ex12.rb
@@ -0,0 +1,13 @@
+print "Give me a number: "
+# convert the input into integer with .to_i
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
